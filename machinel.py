968951e3c19a752d61f0178bfb0758b2738ef97e
Parshva Shah <parshvashah414@gmail.com>
2019-05-19 13:05:15 -0400

    Add files via upload

---
+++ machinel.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 11:38:59 2019
+
+@author: parshvashah
+"""
+
+from sklearn import tree
+features = [[140,1],[130,1],[150,0],[170,0]]
+labels = [0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features,labels)
+print (clf.predict([[150,0]]))
